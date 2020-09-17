d1592dd60bf72ff0b400c1c0b2f0e0119cec2f7e
Cai Nowicki <cai.nowicki@gmail.com>
2020-09-16 23:35:36 -0400

    started working on using linkedlist for stack

--- stack/stack.py
+++ stack/stack.py
@@ -29,5 +29,32 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import LinkedList
 
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        copy = self
+        try:
+            copy.storage.head.get_value()
+            count = 1
+        except AttributeError:
+            return 0
+        while copy.storage.head.get_next_node():
+            copy.storage.remove_head()
+            count += 1
+        return count
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+
+    def pop(self):
+        if self.__len__() == 0:
+            return None
+        else:
+            return self.storage.remove_tail()
 
