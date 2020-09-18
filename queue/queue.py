d31c301dcdae43d0552503b7e36fd69079f623ca
Cai Nowicki <cai.nowicki@gmail.com>
2020-09-17 22:32:33 -0400

    queue working with linked list

--- queue/queue.py
+++ queue/queue.py
@@ -13,19 +13,40 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         return sum([1 for i in self.storage])
+#
+#     def enqueue(self, value):
+#         self.storage.append(value)
+#
+#     def dequeue(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop(0)
+from singly_linked_list import Node, LinkedList
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        return sum([1 for i in self.storage])
+        return self.size
 
     def enqueue(self, value):
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        if len(self.storage) == 0:
+        if self.size == 0:
             return None
         else:
-            return self.storage.pop(0)
+            self.size -= 1
+            return self.storage.remove_head()
